ae6bfb6f5cbffc6bef437c81f54f2abc27e01ccd
Andy Maskiell <maskiella@gmail.com>
2018-01-31 18:19:36 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -8,5 +8,31 @@ class Dog
     @breed = breed
   end
 
+  def self.create_table
+  end
 
+  def self.drop_table
+  end
+
+  def save
+  end
+
+  def self.create
+  end
+
+  def self.find_by_id
+  end
+
+  def self.find_or_create_by
+  end
+
+  def self.new_from_db
+  end
+
+  def self.find_by_name
+  end
+
+  def update
+  end
+  
 end
