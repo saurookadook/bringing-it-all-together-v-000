c5c1a055280f7ee2170998091f339e03b667f3ac
Andy Maskiell <maskiella@gmail.com>
2018-01-31 19:04:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -51,7 +51,7 @@ class Dog
     sql = "SELECT * FROM dogs WHERE id = ?"
     binding.pry
     result = DB[:conn].execute(sql, id)[0]
-    Dog.new(result[0], result[1], result[2])
+    Dog.new(id: result[0], name: result[1], breed: result[2])
   end
 
   def self.find_or_create_by(name:, breed:)
