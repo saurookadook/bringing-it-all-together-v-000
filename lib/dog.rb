654353122db502cff926b722fa92869aac958f12
Andy Maskiell <maskiella@gmail.com>
2018-01-31 18:41:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,6 +68,9 @@ class Dog
   end
 
   def self.find_by_name(name)
+    sql = "SELECT * FROM dogs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Dog.new(result[0], result[1], result[2])
   end
 
   def update
